482d71eed48f9d7ff0f8d401fd2ddae4354a81df
adamGolczynski <agolczynski97@gmail.com>
2024-07-12 22:15:29 +0200

    added: new name for main function and fix answer text cleaning

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const ballImg = document.querySelector('.ball-img img')
-const input = document.querySelector('.question-area input')
-const answerText = document.querySelector('.answer')
-const errorText = document.querySelector('.error')
+const ballImg = document.querySelector('.ball-img img');
+const input = document.querySelector('.question-area input');
+const answerText = document.querySelector('.answer');
+const errorText = document.querySelector('.error');
 
 const answersArray = [
 	'Tak!',
@@ -13,30 +13,32 @@ const answersArray = [
 	'Może!',
 	'Tego to chyba nikt nie wie!',
 	'Oczywiście!',
-]
+];
 
 const generateRandomAnswer = answersArray => {
-	errorText.textContent = ''
-	const randomNumber = Math.floor(Math.random() * answersArray.length)
-	answerText.innerHTML = `<span>Odpowiedź:</span> ${answersArray[randomNumber]}`
-}
+	errorText.textContent = '';
+	const randomNumber = Math.floor(Math.random() * answersArray.length);
+	answerText.innerHTML = `<span>Odpowiedź:</span> ${answersArray[randomNumber]}`;
+};
 
 const inputCheck = inputValue => {
 	if (inputValue === '') {
-		errorText.textContent = 'Musisz zadać jakieś pytanie!'
+		errorText.textContent = 'Musisz zadać jakieś pytanie!';
+		answerText.textContent = '';
 	} else if (inputValue.at(-1) !== '?') {
-		errorText.textContent = 'Pytanie musi być zakończone znakiem "?"'
+		errorText.textContent = 'Pytanie musi być zakończone znakiem "?"';
+		answerText.textContent = '';
 	} else {
-		generateRandomAnswer(answersArray)
+		generateRandomAnswer(answersArray);
 	}
-}
+};
 
-const handler = () => {
-	ballImg.classList.add('shake-animation')
-	inputCheck(input.value)
-}
+const shakeBall = () => {
+	ballImg.classList.add('shake-animation');
+	inputCheck(input.value);
+};
 
-ballImg.addEventListener('click', handler)
+ballImg.addEventListener('click', shakeBall);
 ballImg.addEventListener('animationend', () => {
-	ballImg.classList.remove('shake-animation')
-})
+	ballImg.classList.remove('shake-animation');
+});
